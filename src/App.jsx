afc6165a5b1e0f647ff6f417d886a91c2cee7e36
SlayerSj22 <112235877+SlayerSj22@users.noreply.github.com>
2024-01-19 23:19:31 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h4>A blogsite with appwrite by shashwat jain mentor hitesh chaudhry</h4>
+    </>
+  )
+}
+
+export default App
