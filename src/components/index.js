faa63876b56ce23bc5b9e7e9bed1bf29c6c7e6d0
SlayerSj22 <112235877+SlayerSj22@users.noreply.github.com>
2024-01-24 11:46:31 +0530

    more components

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,17 @@ import Footer from "./Footer/Footer";
 import Container from './Container/Container'
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 
 export {
     
        Header,
        Footer,
        Container,
-       Logo
+       Logo,
+       Button,
+       Input
 
 
 
